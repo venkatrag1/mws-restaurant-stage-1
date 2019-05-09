dc988ef4b73a4a89e94c67cad9dbeeb150ae2994
venkatrag1 <venkatraghavan.r@gmail.com>
2019-05-09 23:38:58 +0000

    register sw

---
+++ js/swhelper.js
@@ -0,0 +1,7 @@
+(function () {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(() => { console.log("Registered service worker"); })
+        .catch(() => { console.log("Failed to register service worker"); });
+  }
+})();
